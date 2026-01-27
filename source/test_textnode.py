5155dc07b97b7f19ae52baa4ff2d11d7a06690c7
Tracermk31 <tracermk31@gmail.com>
2026-01-27 08:58:44 -0700

    B. Added test cases

---
+++ source/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_type_ineq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.PLAIN)
+        self.assertNotEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.url, node2.url)
+
+    def test_url_is_not_none(self):
+        node = TextNode("This is a text node", TextType.BOLD, url ="Blah")
+        self.assertTrue(node.url != None)
+
+if __name__ == "__main__":
+    unittest.main()
